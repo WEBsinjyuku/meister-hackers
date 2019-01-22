b84e1a5d0dd251e28a3f26b6eb8c275986d776c5
takaaa220 <taaaaaaaaaa220@gmail.com>
2019-01-22 21:41:22 +0900

    refs #52 settings for auto generation

--- config/application.rb
+++ config/application.rb
@@ -40,6 +40,9 @@ module GithubOpenProducts
                         controller_specs: true,
                         request_specs: false
       g.fixture_replacement :factory_bot, dir: "spec/factories"
+      g.assets false
+      g.helper false
+      g.jbuilder false
     end
   end
 end
