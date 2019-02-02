40955c219949f8c8b97737107c8b0b7405cb2616
tsu-nera <fox10225fox@gmail.com>
2019-02-02 20:27:26 +0900

    docker環境で動かす

--- Gemfile
+++ Gemfile
@@ -65,6 +65,7 @@ group :development do
   gem "spring-watcher-listen", "~> 2.0.0"
 
   gem "better_errors"
+  gem "binding_of_caller"
 end
 
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,4 +60,8 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
 end
+
+# for better errors
+BetterErrors::Middleware.allow_ip! "0.0.0.0/0"
