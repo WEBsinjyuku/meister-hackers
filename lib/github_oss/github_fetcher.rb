ca297a3d46292415ada5ac747d48462539a77310
tsu-nera <fox10225fox@gmail.com>
2019-02-11 12:03:53 +0900

    base

--- lib/github_oss/github_fetcher.rb
+++ lib/github_oss/github_fetcher.rb
@@ -2,5 +2,22 @@
 
 module GithubOss
   class GithubFetcher
+    def initialize(repo)
+      @repo = repo
+    end
+
+    private
+
+      def repo
+        @repo ||= fetch_repo
+      end
+
+      def fetch_repo
+        Octokit.repo @repo
+      rescue StandardError => e
+        # エラー処理はとりあえずログするだけにしておく
+        logger.error(e)
+        {}
+      end
   end
 end
