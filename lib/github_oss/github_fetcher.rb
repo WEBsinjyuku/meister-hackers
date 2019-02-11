cc5827aa2142f8043b9eeda68d39be9abb6200a3
tsu-nera <fox10225fox@gmail.com>
2019-02-11 12:29:22 +0900

    finish

--- lib/github_oss/github_fetcher.rb
+++ lib/github_oss/github_fetcher.rb
@@ -2,18 +2,43 @@
 
 module GithubOss
   class GithubFetcher
-    def initialize(repo)
-      @repo = repo
+    def initialize(repo_name)
+      @repo_name = repo_name
+    end
+
+    def language
+      repo[:language]
+    end
+
+    def languages
+      @languages ||= Octokit.languages @repo_name
+    end
+
+    def topics
+      @topics ||= Octokit.topics @repo_name
+    end
+
+    def description
+      repo[:description]
+    end
+
+    def stargazers_count
+      repo[:stargazers_count]
+    end
+
+    def collaborators
+      @collab ||= Octokit.collaborators @repo_name
     end
 
     private
+      attr_reader :repo
 
       def repo
         @repo ||= fetch_repo
       end
 
       def fetch_repo
-        Octokit.repo @repo
+        Octokit.repo @repo_name
       rescue StandardError => e
         # エラー処理はとりあえずログするだけにしておく
         logger.error(e)
