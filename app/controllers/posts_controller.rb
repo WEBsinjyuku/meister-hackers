dd146c51a2fdabc6b4e2a6451ce1a67b4bcc67c3
LeonardoMatusaki <leonardo@LeonardoMatusakinoMacBook-Pro.local>
2019-02-13 13:48:01 +0900

    Create prototype of screen design only, have it checked once

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -29,6 +29,10 @@ class PostsController < ApplicationController
     end
   end
 
+  def show
+
+  end
+
   private
     def post_params
       params.require(:post).permit(:title, :content, :repository)
