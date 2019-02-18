a4b9810e3b2381508db6a3a056d1e5934efc83dd
高橋 <icasenyou2018@gmail>
2019-02-18 13:24:10 +0900

    sider構文指摘箇所修正

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,13 +1,12 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
-    def github
-      @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
-  
-      if @user.persisted?
-        sign_in_and_redirect @user, :event => :authentication
-        set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
-      else
-        session["devise.github_data"] = request.env["omniauth.auth"]
-        redirect_to new_user_registration_url
-      end
+  def github
+    @user = User.find_for_github_oauth(request.env["omniauth.auth"], current_user)
+    if @user.persisted?
+      sign_in_and_redirect @user, :event => :authentication
+      set_flash_message(:notice, :success, :kind => "Github") if is_navigational_format?
+    else
+      session["devise.github_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
     end
   end
+end
