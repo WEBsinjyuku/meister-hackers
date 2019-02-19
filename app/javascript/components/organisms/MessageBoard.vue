899cca9260e0d2b4a8b7d5d37bf6579caa50cbb9
tsu-nera <fox10225fox@gmail.com>
2019-02-19 20:54:10 +0900

    list

--- app/javascript/components/organisms/MessageBoard.vue
+++ app/javascript/components/organisms/MessageBoard.vue
@@ -3,5 +3,31 @@
   .tile.is-vertical.box.is-6
     .title.has-background-grey-lighter フィード
     input.input(type="text" placeholder="コメント")
-    .button.is-info Button
+    .button.is-info 投稿
+    ul
+      li(v-for="message in messages" :key="message.id")
+        | {{ message.text }}
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      messages: [
+        {
+          id: 1,
+          text: "こんにちは",
+        },
+        {
+          id: 2,
+          text: "はじめまして、よろしくおねがいします。",
+        },
+        {
+          id: 3,
+          text: "すみません、遅れます。。。",
+        },
+      ],
+    };
+  },
+};
+</script>
