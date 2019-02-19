49e901ef8eaf533d4de9b871d0679a7b1a7011f9
tsu-nera <fox10225fox@gmail.com>
2019-02-20 01:16:40 +0900

    annotate導入

--- spec/factories/messages.rb
+++ spec/factories/messages.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :bigint(8)        not null, primary key
+#  content    :string
+#  post_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
 FactoryBot.define do
   factory :message do
     content { "こんにちは" }
--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,5 +1,17 @@
 # frozen_string_literal: true
 
+# == Schema Information
+#
+# Table name: messages
+#
+#  id         :bigint(8)        not null, primary key
+#  content    :string
+#  post_id    :bigint(8)
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+
+
 require "rails_helper"
 
 RSpec.describe Message, type: :model do
