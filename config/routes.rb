4cd2bafe7c28bab52111e76ea7bf3935e706b3ec
高橋 <icasenyou2018@gmail>
2019-02-20 00:44:10 +0900

    usersController生成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/show'
   devise_for :users, controllers: {
     registrations: "users/registrations",
     omniauth_callbacks: "users/omniauth_callbacks"
