5efade73857111989474c39554e685668a2718fe
tsu-nera <fox10225fox@gmail.com>
2019-02-19 19:53:12 +0900

    update

--- db/schema.rb
+++ db/schema.rb
@@ -26,6 +26,8 @@ ActiveRecord::Schema.define(version: 2019_02_14_131932) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
     t.string "name", default: "", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -38,8 +40,6 @@ ActiveRecord::Schema.define(version: 2019_02_14_131932) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string "unconfirmed_email"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["name"], name: "index_users_on_name", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
