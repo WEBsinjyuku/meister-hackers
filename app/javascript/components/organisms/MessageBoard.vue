59c0b9bc78931c0afd2aca90f45022ad96bc144e
tsu-nera <fox10225fox@gmail.com>
2019-02-19 20:39:50 +0900

    add button

--- app/javascript/components/organisms/MessageBoard.vue
+++ app/javascript/components/organisms/MessageBoard.vue
@@ -1,5 +1,7 @@
 <template lang="pug">
 .tile.is-ancestor
   .tile.is-vertical.box.is-6
-    .title フィード
+    .title.has-background-grey-lighter フィード
+    input.input(type="text" placeholder="コメント")
+    .button.is-info Button
 </template>
