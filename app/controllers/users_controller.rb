ad8d43f0184a7a79959a571d22c9b728f6a00741
高橋 <icasenyou2018@gmail>
2019-02-20 15:27:57 +0900

    showのview、左辺テキスト部分調整前

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,13 @@
 class UsersController < ApplicationController
+  # ログインユーザーしかみられないように実装
   def show
-    @user = User.find(params[:id])
+    # @user = User.find(params[:id])
+    # →Couldn't find User without an IDエラーで動かない
+    # @user = User.find_by(id: params[:id])
+    # →undefined method `name' for nil:NilClass
+    @user = User.find_by(params[:id])
+    # このの書き方だと動くがidが常に1のユーザーになる。id２のユーザーでログインしても同じid:
+    # @user = User.all
+    # →これは問題なく表示される
   end
 end
