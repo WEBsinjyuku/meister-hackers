fff11be939d3ce487f69db673ba9ccaf819c0878
tsu-nera <fox10225fox@gmail.com>
2019-02-21 13:22:46 +0900

    整えた

--- app/javascript/components/molecules/UserProfile.vue
+++ app/javascript/components/molecules/UserProfile.vue
@@ -5,20 +5,22 @@
       img(src="https://avatars3.githubusercontent.com/u/760627?s=460&v=4" alt="Placeholder image")
   .card-content
     .content
-      a.title
+      .title
         | tsu-nera
-      p
-        | GitHub
-      p
-        | Twitter
-      p
-        | Facebook
-      p
-        | Blog  
-      p
-        | 都道府県： 東京都
-      p 
-        | 性別：男
-      p
-        | 登録日:2019/1/1
+      .list
+        .list-item
+          | GitHub
+        .list-item
+          | Twitter
+        .list-item
+          | Facebook
+        .list-item
+          | Blog  
+        .list-item
+          | 都道府県： 東京都
+        .list-item
+          | 性別：男
+        .list-item
+          | 登録日:2019/1/1
+      .button.is-primary 編集
 </template>
