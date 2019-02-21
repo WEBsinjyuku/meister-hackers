14abb6d7a8d053d107fa2a782e6f67f1ae0e6910
tsu-nera <fox10225fox@gmail.com>
2019-02-21 12:27:56 +0900

    vuejs移行

---
+++ app/javascript/packs/users/show.vue.js
@@ -0,0 +1,9 @@
+import Vue from "vue/dist/vue.esm";
+import MyPage from "../../components/organisms/MyPage.vue";
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: "#mypage",
+    render: h => h(MyPage),
+  });
+});                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
