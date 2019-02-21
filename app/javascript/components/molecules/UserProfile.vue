8640488bf8f09e394d0da2011b4c0d5456581c09
tsu-nera <fox10225fox@gmail.com>
2019-02-21 14:05:21 +0900

    font-awesome導入

--- app/javascript/components/molecules/UserProfile.vue
+++ app/javascript/components/molecules/UserProfile.vue
@@ -9,18 +9,18 @@
         | tsu-nera
       .list
         .list-item
-          | GitHub
-        .list-item
-          | Twitter
-        .list-item
-          | Facebook
-        .list-item
-          | Blog  
-        .list-item
           | 都道府県： 東京都
         .list-item
           | 性別：男
         .list-item
-          | 登録日:2019/1/1
-      .button.is-primary 編集
+          | 登録日: 2019/01/01
+      .icon.is-large
+        .fab.fa-2x.fa-github
+      .icon.is-large
+        .fab.fa-2x.fa-twitter
+      .icon.is-large
+        .fab.fa-2x.fa-facebook
+      .icon.is-large
+        .fas.fa-2x.fa-rss-square
+    .button.is-primary 編集
 </template>
