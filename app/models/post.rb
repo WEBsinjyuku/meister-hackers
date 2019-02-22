aeb49aa3a20d47fe63918d0c9a11779f1d7c27db
廣本 和也 <hiromoto.kazuya@gmail.com>
2019-02-22 13:40:49 +0900

    before_saveでレポジトリURLを(/ドメイン/リポジトリ名)の形に整形

--- app/models/post.rb
+++ app/models/post.rb
@@ -7,4 +7,10 @@ class Post < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true
   validates :repository, presence: true
+
+  before_save :format_repository_url
+
+  def format_repository_url
+    self.repository.gsub!(/https:\/\/github.com/, '')
+  end
 end
