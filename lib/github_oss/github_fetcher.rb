4a72f637040a3c2b811556e22d775503eda00c71
廣瀬 <dhirose12345@gmail.com>
2019-02-23 17:30:21 +0900

    遷移スクリプト修正

--- lib/github_oss/github_fetcher.rb
+++ lib/github_oss/github_fetcher.rb
@@ -3,9 +3,9 @@
 module GithubOss
   class GithubFetcher
     def initialize(repo_name)
-      # 仮認証
-      Octokit.login = "自分のアカウント名"
-      Octokit.password = "パスワード"
+      # 仮認証(Basic認証)
+      #Octokit.login = "XXXX"
+      #Octokit.password = "XXXX"
       @repo_name = repo_name
     end
 
