3ba717b50bee2af5e7997af3accaccc6b7b0c15f
廣本 和也 <hiromoto.kazuya@gmail.com>
2019-02-24 16:15:21 +0900

    githubログイン実装済につき不要ファイルを削除

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,6 @@ Rails.application.routes.draw do
   }
   # 後ほど変更
   root :to => 'posts#index'
-  # root 'sessions#test'
-  # ログイン機能実装後削除
-  post '/login_dev', to: 'sessions#login_dev'
-
-  delete '/logout', to: 'sessions#destroy'
 
   resources :posts, only: %i(new create edit update show index) do
     resources :messages, only: %i(index create)
