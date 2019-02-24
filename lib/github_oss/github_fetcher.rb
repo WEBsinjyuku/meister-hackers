188445f62884e085ff175cac91a037a25708d3e4
廣瀬 <dhirose12345@gmail.com>
2019-02-25 03:28:54 +0900

    Sider指摘修正

--- lib/github_oss/github_fetcher.rb
+++ lib/github_oss/github_fetcher.rb
@@ -4,8 +4,8 @@ module GithubOss
   class GithubFetcher
     def initialize(repo_name)
       # 仮認証(Basic認証)
-      #Octokit.login = "XXXX"
-      #Octokit.password = "XXXX"
+      # Octokit.login = "XXXX"
+      # Octokit.password = "XXXX"
       @repo_name = repo_name
     end
 
