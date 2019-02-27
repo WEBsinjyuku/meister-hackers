c858aa3f48b01d53daab219638f25f5944d8a449
tsu-nera <fox10225fox@gmail.com>
2019-02-28 06:34:38 +0900

    tsu-nera check

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -27,7 +27,7 @@ FactoryBot.define do
     sequence (:name) { |n| "tsu-nera#{n}" }
     sequence(:email) { |n| "example#{n}@gmail.com" }
     password { "helloworld" }
-    sequence(:avatar_url) { |n| "sample_url" }
+    avatar_url { "sample_url" }
     sequence(:uid) { |n| n }
     provider { "github" }
   end
