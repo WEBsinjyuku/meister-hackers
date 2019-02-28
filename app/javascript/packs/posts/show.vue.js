f84ff71fab5ed438481cdd31a8f26102fe3feb88
tsu-nera <fox10225fox@gmail.com>
2019-02-28 20:55:48 +0900

    wip

--- app/javascript/packs/posts/show.vue.js
+++ app/javascript/packs/posts/show.vue.js
@@ -1,9 +1,43 @@
 import Vue from "vue/dist/vue.esm";
+import Axios from "axios";
+
 import MessageBoard from "../../components/organisms/MessageBoard.vue";
+import UserProfile from "../../components/molecules/UserProfile.vue";
 
 document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#message-board",
     render: h => h(MessageBoard),
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  new Vue({
+    el: "#user-profile",
+    render: h => h(UserProfile),
+    data() {
+      return {
+        user: {
+          id: 0,
+          name: "",
+          avatar: "",
+          isLogin: false,
+        },
+        profile: null,
+      };
+    },
+    mounted() {
+      const profileUrl = "/users/17/profiles";
+      Axios.get(profileUrl)
+        .then((response) => {
+          this.user.name = response.data.username;
+          this.user.id = response.data.id;
+          this.user.avatar = response.data.avatar;
+          this.user.isLogin = response.data.is_login;
+          if (response.data.profile) {
+            this.profile = response.data.profile;
+          }
+        });
+    },
   });
 });
