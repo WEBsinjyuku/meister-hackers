860a7df8d39bb2da5e30ecdfe6045b5667f9ee36
tsu-nera <fox10225fox@gmail.com>
2019-03-09 21:01:53 +0900

    fix error

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -2,37 +2,43 @@
 
 class ProfilesController < ApplicationController
   protect_from_forgery except: ["update"]
-  before_action :set_profile, only: [:update]
-  before_action :correct_user, only: [:index, :update]
+  before_action :set_user, only: ["index", "index2"]
 
   def index
-    user = User.find_by(id: params[:user_id])
-    if user
-      render json: { status: 200, profile: user.profile, username: user.name, avatar: user.avatar_url, id: user.id, is_login: user_signed_in? }
-    elsif user.nil?
-      render json: { status: 404 }
+    if @user
+      render json: { status: 200, profile: @user.profile, username: @user.name, avatar: @user.avatar_url, id: @user.id, is_login: user_signed_in?, is_current_user: current? }
     else
       render json: { status: 500 }
     end
   end
 
+  def index2
+    if current_user?
+      render json: { status: 200, profile: @user.profile, username: @user.name, avatar: @user.avatar_url, id: @user.id, is_login: user_signed_in?, is_current_user: current? }
+    else
+      render json: { status: 401 }
+    end
+  end
+
   def update
     if params[:id] == "undefined"
-      @profile.area = params[:area]
-      @profile.sex = params[:sex]
-      @profile.github_url = params[:github_url]
-      @profile.twitter_url = params[:twitter_url]
-      @profile.facebook_url = params[:facebook_url]
-      @profile.blog_url = params[:blog_url]
-      @profile.introduction = params[:introduction]
-      @profile.user_id = params[:user_id]
+      profile = Profile.new
+      profile.area = params[:area]
+      profile.sex = params[:sex]
+      profile.github_url = params[:github_url]
+      profile.twitter_url = params[:twitter_url]
+      profile.facebook_url = params[:facebook_url]
+      profile.blog_url = params[:blog_url]
+      profile.introduction = params[:introduction]
+      profile.user_id = params[:user_id]
 
-      if @profile.save
+      if profile.save
         render json: { status: 200 }
       else
         render json: { status: 500 }
       end
     else
+      profile = Profile.find_by(id: params[:id])
       attributes = {
         area: params[:area],
         sex: params[:sex],
@@ -42,7 +48,7 @@ class ProfilesController < ApplicationController
         blog_url: params[:blog_url],
         introduction: params[:introduction]
       }
-      if @profile.update(attributes)
+      if profile.update(attributes)
         render json: { status: 200 }
         flash[:notice] = "編集に成功しました"
       else
@@ -52,20 +58,11 @@ class ProfilesController < ApplicationController
   end
 
   private
+  def set_user
+    @user = User.find_by(id: params[:user_id])
+  end
 
-    def set_profile
-      if params[:id] == "undefined"
-        @profile = Profile.new
-      else
-        @profile = Profile.find_by(id: params[:id])
-      end
-    end
-
-    def correct_user
-      if @profile.nil?
-        render json: { status: 401 }
-      elsif current_user.id == @profile.user.id
-        render json: { status: 401 }
-      end
-    end
+  def current?
+    @user.present? && current_user.present? && current_user.id == @user.id
+  end
 end
