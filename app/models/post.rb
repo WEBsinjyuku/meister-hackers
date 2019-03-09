dfc24c70e1b1e63acd530465aca1e59351793935
tsu-nera <fox10225fox@gmail.com>
2019-03-09 21:10:34 +0900

    annotate

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,20 @@
 # frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: posts
 #
-#  id         :bigint(8)        not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  title      :string           not null
-#  content    :text             not null
-#  user_id    :bigint(8)        not null
-#  repository :string           not null
-#  status     :integer          default("wanted"), not null
-#  owner      :string
-#  opened_on  :date
-#  closed_on  :date
+#  id            :bigint(8)        not null, primary key
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  title         :string           not null
+#  content       :text             not null
+#  user_id       :bigint(8)        not null
+#  repository    :string           not null
+#  status        :integer          default("wanted"), not null
+#  owner         :string           not null
+#  opened_on     :date
+#  closed_on     :date
+#  slack_channel :string
 #
 
 class Post < ApplicationRecord
--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,18 +1,20 @@
 # frozen_string_literal: true
-
 # == Schema Information
 #
 # Table name: posts
 #
-#  id         :bigint(8)        not null, primary key
-#  created_at :datetime         not null
-#  updated_at :datetime         not null
-#  title      :string           not null
-#  content    :text             not null
-#  user_id    :bigint(8)        not null
-#  repository :string           not null
-#  status     :integer          default("wanted"), not null
-#  owner      :string
+#  id            :bigint(8)        not null, primary key
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  title         :string           not null
+#  content       :text             not null
+#  user_id       :bigint(8)        not null
+#  repository    :string           not null
+#  status        :integer          default("wanted"), not null
+#  owner         :string           not null
+#  opened_on     :date
+#  closed_on     :date
+#  slack_channel :string
 #
 
 FactoryBot.define do
