6467bd078393e808c383bbed4752a4f3ef20cd19
shikky0331 <shikky0335@gmail.com>
2019-03-09 14:23:07 +0900

    mypageform.vue 途中

--- app/javascript/components/organisms/MyPageForm.vue
+++ app/javascript/components/organisms/MyPageForm.vue
@@ -2,35 +2,36 @@
 .bd-content
   .content
     .columns.is-centered
-      .column.is-half
-        form(@submit.prevent="submit")
-          .field
-            .label 都道府県
-            input.input(type="text" v-model="profile.area")
-          .field
-            .label 性別
-            label.radio 男性
-              input(type="radio" name="sex" value="男性" v-model="profile.sex")
-            label.radio 女性
-              input(type="radio" name="sex" value="女性" v-model="profile.sex")
-          .field
-            .label GitHub URL
-            input.input(type="text" v-model="profile.github_url")
-          .field
-            .label Twitter URL
-            input.input(type="text" v-model="profile.twitter_url")
-          .field
-            .label Facebook URL
-            input.input(type="text" v-model="profile.facebook_url")
-          .field
-            .label Blog URL
-            input.input(type="text" v-model="profile.blog_url")
-          .field
-            .label 自由記述文
-            textarea.textarea(v-model="profile.introduction")
-          .field
-            button.button(type="submit").is-primary 編集完了
-            button.button(type="button" @click="cancel") キャンセル
+      form(@submit.prevent="submit")
+        .account-header
+          .account-header-left アカウント名
+          .account-header-right
+            .cancel-button(type="button" @click="cancel") キャンセル
+        .account アカウント
+        .field
+          .label 都道府県
+          input.input(type="text" v-model="profile.area")
+        .field
+          .label 性別
+          input.input(type="text" v-model="profile.sex")
+        .field
+          .label 登録日
+        .field
+          .label 自己紹介
+          input.input(v-model="profile.introduction")
+        .field
+          .label GitHub URL
+          input.input(type="text" v-model="profile.github_url")
+        .field
+          .label Twitter URL
+          input.input(type="text" v-model="profile.twitter_url")
+        .field
+          .label Facebook URL
+          input.input(type="text" v-model="profile.facebook_url")
+        .field
+          .label Blog URL
+          input.input(type="text" v-model="profile.blog_url")
+        button.button(type="submit").is-primary 更新
 </template>
 
 <script>
@@ -78,3 +79,74 @@ export default {
   },
 };
 </script>
+
+<style scoped>
+  body {
+    background-color: white;
+  }
+
+  .field {
+    display: flex;
+    justify-content: space-between;
+    border-bottom: solid 1px #f9fbfe;
+    padding-bottom: 10px;
+  }
+
+  .account-header {
+    height: 70px;
+    width: 100vw;
+    margin-left: calc(50% - 50vw);
+    display: flex;
+    justify-content: space-between;
+    background-color: #F0FFFF;
+  }
+
+  .account-header-left {
+    font-size: 1.5rem;
+    margin-left: 250px;
+    line-height: 75px;
+  }
+
+  .account-header-right {
+    margin-right: 250px;
+    line-height: 75px;
+  }
+
+  .cancel-button {
+    font-size: 1.2rem;
+    color: #00d1b2;
+    cursor: pointer;
+  }
+
+  .account {
+    font-weight: bold;
+    width: 80px;
+    margin-top: 20px;
+    margin-bottom: 20px;
+    border-bottom: solid 3px black;
+  }
+
+  .label {
+    font-weight: normal;
+    margin-right: 30px;
+  }
+
+  .input {
+    width: 800px;
+  }
+
+  form {
+    width: 100%;
+  }
+
+  .button {
+    display: block;
+    font-size: 0.8rem;
+    font-weight: bold;
+    margin-left: auto;
+    color: white;
+    background-color: red;
+    border-radius: 10px;
+  }
+
+</style>
