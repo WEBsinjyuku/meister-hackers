66b26300066498495fccdd1720eeeb6931f8d4f9
shikky0331 <shikky0335@gmail.com>
2019-03-11 15:25:14 +0900

    posts_edit & new & show & index title add

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,15 +1,33 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
-  def get_status_name(status)
-    status_name = ""
-    case status
-    when "wanted"
-      status_name = "【募集中】"
-    when "stopped"
-      status_name = "【募集終了】"
-    else
-    end
-    status_name
+  # application_helper.rb
+  def default_meta_tags
+    {
+      site: 'Meister Hackers',
+      reverse: true,
+      separator: '|',
+      og: defalut_og,
+      twitter: default_twitter_card
+    }
   end
-end
+
+  private
+
+    def defalut_og
+      {
+        title: :full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
+        description: :description,   # 上に同じ
+        url: request.url,
+        image: 'https://example.com/hoge.png'
+      }
+    end
+
+    def default_twitter_card
+      {
+        card: 'summary_large_image', # または summary
+        site: '@hogehoge'            # twitter ID
+      }
+    end
+
+  end
