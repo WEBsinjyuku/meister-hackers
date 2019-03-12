b74e2c91cc6f842b15dfd52c41dc3c741fa5cb77
shikky0331 <shikky0335@gmail.com>
2019-03-12 12:12:11 +0900

    slider指摘修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,9 +4,11 @@ module ApplicationHelper
   # application_helper.rb
   def default_meta_tags
     {
-      site: 'Meister Hackers',
+      site: "Meister Hackers",
+      title: "",
+      description: "description",
       reverse: true,
-      separator: '|',
+      separator: "|",
       og: defalut_og,
       twitter: default_twitter_card
     }
@@ -17,17 +19,20 @@ module ApplicationHelper
     def defalut_og
       {
         title: :full_title,          # :full_title とすると、サイトに表示される <title> と全く同じものを表示できる
-        description: :description,   # 上に同じ
-        url: request.url,
-        image: 'https://example.com/hoge.png'
+        description: "本物の仲間達と市場価値を高め合う、エンジニア達のプラットフォーム",   # 上に同じ
+        url: "https://meister-hackers.herokuapp.com",
+        image: "https://example.com/hoge.png"
       }
     end
 
     def default_twitter_card
       {
-        card: 'summary_large_image', # または summary
-        site: '@hogehoge'            # twitter ID
+        card: "summary_large_image", # または summary
+        site: "@PROs08364820", # twitter ID
+        url: "https://meister-hackers.herokuapp.com",
+        title: "Meister Hackers title",
+        description: "本物の仲間達と市場価値を高め合う、エンジニア達のプラットフォーム",
+        image: "https://github.com/WEBsinjyuku/meister-hackers/blob/master/public/Meister%20Hackers.png"
       }
     end
-
   end
