3b3a000c65d24d2946ad8a9a15da025a5d2eec60
廣瀬 <dhirose12345@gmail.com>
2019-03-17 17:37:15 +0900

    マイグレーションファイル修正

--- db/migrate/20190316230307_add_column_to_messages.rb
+++ db/migrate/20190316230307_add_column_to_messages.rb
@@ -1,5 +1,5 @@
 class AddColumnToMessages < ActiveRecord::Migration[5.2]
   def change
-    add_column :messages, :user_id, :bigint, limit: 8
+    add_column :messages, :user_id, :bigint
   end
 end
